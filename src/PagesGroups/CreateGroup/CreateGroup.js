cb88b70e444bdc35a2b0951f89c146f09abf0d4c
unknown <kamilplewka12a@wp.pl>
2023-09-17 12:47:24 +0200

    user can create a group add metting and payment

--- src/PagesGroups/CreateGroup/CreateGroup.js
+++ src/PagesGroups/CreateGroup/CreateGroup.js
@@ -10,40 +10,68 @@ import {
   Button,
 } from "../../Components/variables";
 import "firebase/auth";
-import { Formik } from "formik";
+import { Formik, Field } from "formik";
+import DatePicker from "react-datepicker";
+import { format } from "date-fns"; // Importuj funkcję format z date-fns
+import "react-datepicker/dist/react-datepicker.css";
 import "./createGroup.css";
 import { getAuth } from "firebase/auth";
 import { createCollectionAndAddDocument } from "../../firebase";
 import Navigation from "../../Components/Navigation/Navigation";
+import { useState } from "react";
+
+const DatePickerField = ({ field, form, ...props }) => {
+  return (
+    <DatePicker
+      {...field}
+      {...props}
+      selected={(field.value && new Date(field.value)) || null}
+      onChange={(val) => {
+        form.setFieldValue(field.name, val);
+      }}
+    />
+  );
+};
+
 function CreateGroup() {
   const navigate = useNavigate();
   const auth = getAuth();
+  const [startDate, setStartDate] = useState(new Date());
 
   return (
     <>
       <Navigation />
-      <section className=" section-create_group">
+      <section className="section-create_group">
         <header className="section-create_group_Header">
           <img
             onClick={() => {
               navigate(-1);
             }}
-            className="section-create_group_Header_arrow "
+            className="section-create_group_Header_arrow"
             src={arrow}
             alt="arrow left"
           />
           <MediumTitle className="section-create_group_title">
-            Utwórz grupę{" "}
+            Utwórz grupę
           </MediumTitle>
         </header>
         <Formik
-          initialValues={{ subjects: "", deadline: "", description: "" }}
-          onSubmit={(values) =>
+          initialValues={{
+            subjects: "",
+            deadline: "",
+            description: "",
+            date: null,
+            money:null
+          }}
+          onSubmit={(values) => {
+            // Dodano parametr values
+            const newDate = format(new Date(values.date), "dd.MM.yyyy HH:mm");
             createCollectionAndAddDocument("Groups", {
               ...values,
+              date: newDate,
               userUid: auth.currentUser.uid,
-            })
-          }
+            });
+          }}
         >
           {({ values, handleChange, handleSubmit }) => (
             <form
@@ -51,8 +79,10 @@ function CreateGroup() {
               onSubmit={handleSubmit}
             >
               <SmallLabelForm>Tytuł</SmallLabelForm>
-              <InputForm />
+              <InputForm name="title" />
+
               <div className="section-groups_sort_container">
+                {" "}
                 <SelectInput
                   onChange={handleChange}
                   value={values.subjects}
@@ -62,37 +92,61 @@ function CreateGroup() {
                   <option value="">Przedmiot</option>
                   <option value="Matematyka">Matematyka</option>
                 </SelectInput>
-
                 <SelectInput
                   onChange={handleChange}
                   className="section-groups_sort"
                   name="deadline"
-                  value={values.date}
+                  value={values.deadline}
                 >
                   <option value="">Deadline</option>
                   <option value="one week"> one week</option>
                 </SelectInput>
               </div>
+
               <SmallLabelForm>Opis</SmallLabelForm>
-              <textarea
+              <Field
+                as="textarea"
                 onChange={handleChange}
                 value={values.description}
                 className="section-create_group_description"
                 id="description"
                 name="description"
               />
+
               <MedParagraph className="section-create_group_btns">
-                dodaj spotkanie
+                Opcjonalne
               </MedParagraph>
-              <MedParagraph>dodaj wynagrodzenie</MedParagraph>
+
+              <div className="section-create_group_meeting">
+                <MedParagraph>Dodaj spotkanie</MedParagraph>
+                <Field
+                  component={DatePickerField}
+                  name="date"
+                  className="section-create_group_datepicker"
+                  showTimeSelect // Dodano opcję showTimeSelect
+                  dateFormat="dd.MM.yyyy HH:mm" // Format daty i czasu
+                />
+              </div>
+              <SelectInput
+                onChange={handleChange}
+                value={values.money}
+                className="section-groups_sort"
+                name="subjects"
+              >
+                <option value="">Wynagrodzenie</option>
+                <option value="20 zł">20 zł</option>
+                <option value="30 zł">30 zł</option>
+                <option value="40 zł">40 zł</option>
+              </SelectInput>
               <SimpleBlockInput
                 onClick={() => {
-                  navigate("../recommendedTutor");
+                  console.log(values);
                 }}
                 className="addTutor_btn"
               >
                 Zaproś korepetytora
               </SimpleBlockInput>
+
               <div className="buttons_boxes">
                 <Button
                   onClick={() => {
